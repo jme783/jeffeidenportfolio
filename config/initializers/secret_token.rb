792c031dc0aaba070dc31f2e4278ce5ee6c6775a
Jeff Eiden <jeff.m.eiden@gmail.com>
2014-05-16 16:21:11 -0500

    Added rails application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+JeffEidenPortfolio::Application.config.secret_key_base = '6c8e6342aa82479caf45b5f59beb51d21df0c28db11d1f3b7b74242f7ff4c786735739be054df20369629e9a66922a58cdc19ab768f6caab7771bdb13dbad142'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+JeffEidenPortfolio::Application.config.session_store :cookie_store, key: '_jeff_eiden_portfolio_session'
