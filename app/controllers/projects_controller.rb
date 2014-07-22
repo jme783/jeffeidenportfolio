2d56a794b21f1ba33f969747d68f03157a57ffd5
Jeff Eiden <jeff.m.eiden@gmail.com>
2014-07-22 13:37:11 -0300

    Added skills/known technologies to each project

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,6 +1,7 @@
 class ProjectsController < ApplicationController
   def show
     @project = Project.find(params[:id])
+    @known_technologies = @project.known_technologies.order(skill_id: :asc, id: :asc).group_by{|t| t.skill.name}
     respond_to do |format|
       format.js #renders show.js.erb
     end
