959a6f6ff84d054af3356f48ff2de0e12f44e9f5
Jeff Eiden <jeff.m.eiden@gmail.com>
2014-07-28 10:33:53 -0300

    Removed N+1 queries

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,7 @@
 class HomeController < ApplicationController
   def index
     @testimonials = Testimonial.all
-    @skills = Skill.all
+    @skills = Skill.all.includes(:known_technologies)
     @projects = Project.all
     fresh_when last_modified: @testimonials.maximum(:updated_at)
     fresh_when last_modified: @skills.maximum(:updated_at)
--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,6 +1,6 @@
 class ProjectsController < ApplicationController
   def show
-    @project = Project.find(params[:id])
+    @project = Project.find(params[:id]).includes(:known_technologies)
     @known_technologies = @project.known_technologies.order(skill_id: :asc, id: :asc).group_by{|t| t.skill.name}
     respond_to do |format|
       format.js #renders show.js.erb
